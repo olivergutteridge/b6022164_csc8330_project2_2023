01619faf4bfb16fa180cd1744a632df3796c6330
olivergutteridge <olivergutteridge@gmail.com>
2023-02-17 19:37:25 +0000

    translate func

--- dna.py
+++ dna.py
@@ -1,5 +1,6 @@
 from Bio.Seq import Seq
 from Bio import SeqIO
+import os
 
 class DNA:
 
@@ -36,11 +37,30 @@ class DNA:
                     G += 1
             output += f"{sequence.id}\nLength: {len(sequence.seq)}\nA: {A}   ({round(A/len(sequence.seq)*100)}%)\nT: {T}   ({round(T/len(sequence.seq)*100)}%)\nC: {C}   ({round(C/len(sequence.seq)*100)}%)\nG: {G}   ({round(G/len(sequence.seq)*100)}%)\nGC content: {(G+C)/len(sequence.seq)}\n\n"
         return output
+
+    def translate(self, prefix):
+        path = os.path.join("./", f"{prefix}")
+        os.mkdir(path)
+        for sequence in self.sequences:
+            forward = Seq(sequence.seq)
+            reverse = forward.reverse_complement()
+            file = open(f"./{prefix}/{sequence.id}.fa", "w")
+            file.write(">"+sequence.id+"|+1\n")
+            file.write(str(forward.translate()))
+            file.write("\n>"+sequence.id+"|+2\n")
+            file.write(str(forward[1:].translate()))
+            file.write("\n>"+sequence.id+"|+3\n")
+            file.write(str(forward[2:].translate()))
+            file.write("\n>"+sequence.id+"|-1\n")
+            file.write(str(reverse.translate()))
+            file.write("\n>"+sequence.id+"|-2\n")
+            file.write(str(reverse[1:].translate()))
+            file.write("\n>"+sequence.id+"|-3\n")
+            file.write(str(reverse[2:].translate()))
+            file.close()
+        return "Sequences have been translated."
             
             
-
-    
-
 if __name__ == "__main__":
     x = DNA("sequences.fa")
-    print(x.statistics())
+    print(x.translate("test"))
