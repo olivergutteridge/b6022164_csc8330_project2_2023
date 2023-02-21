7619f76a6580a9ae59683936bf5e52f82194dc14
olivergutteridge <olivergutteridge@gmail.com>
2023-02-21 12:55:01 +0000

    update statistics method

--- dna.py
+++ dna.py
@@ -5,57 +5,45 @@ import os
 class DNA:
 
     def __init__(self, file) -> None:
-        self.sequences = []
-        for sequence in SeqIO.parse(file, "fasta"):
-            self.sequences.append(sequence)
+        self.records = list(SeqIO.parse(file, "fasta"))
 
     def __str__(self) -> str:
         n = 0
         output = ""
-        for sequence in self.sequences:
+        for record in self.records:
             n += 1
-            output += f"{n}: {sequence}\n"
+            output += f"{n}:\n{record}\n"
         return output
 
     def statistics(self):
-        n = 0
         output = ""
-        for sequence in self.sequences:
-            A = 0
-            T = 0
-            C = 0
-            G = 0
-            n += 1
-            for base in sequence:
-                if base == "A":
-                    A += 1
-                elif base == "T":
-                    T += 1
-                elif base == "C":
-                    C += 1
-                elif base == "G":
-                    G += 1
-            output += f"{sequence.id}\nLength: {len(sequence.seq)}\nA: {A}   ({round(A/len(sequence.seq)*100)}%)\nT: {T}   ({round(T/len(sequence.seq)*100)}%)\nC: {C}   ({round(C/len(sequence.seq)*100)}%)\nG: {G}   ({round(G/len(sequence.seq)*100)}%)\nGC content: {(G+C)/len(sequence.seq)}\n\n"
+        for record in self.records:
+            length = len(record.seq)
+            A = record.seq.count("A")
+            T = record.seq.count("T")
+            C = record.seq.count("C")
+            G = record.seq.count("G")
+            output += f"{record.id}\nLength: {length}\nA: {A}   ({round(A/length*100)}%)\nT: {T}   ({round(T/length*100)}%)\nC: {C}   ({round(C/length*100)}%)\nG: {G}   ({round(G/length*100)}%)\nGC content: {(G+C)/length}\n\n"
         return output
 
     def translate(self, prefix):
         path = os.path.join("./", f"{prefix}")
         os.mkdir(path)
-        for sequence in self.sequences:
-            forward = Seq(sequence.seq)
+        for record in self.records:
+            forward = Seq(record.seq)
             reverse = forward.reverse_complement()
-            file = open(f"./{prefix}/{sequence.id}.fa", "w")
-            file.write(">"+sequence.id+"|+1\n")
+            file = open(f"./{prefix}/{record.id}.fa", "w")
+            file.write(">"+record.id+"|+1\n")
             file.write(str(forward.translate()))
-            file.write("\n>"+sequence.id+"|+2\n")
+            file.write("\n>"+record.id+"|+2\n")
             file.write(str(forward[1:].translate()))
-            file.write("\n>"+sequence.id+"|+3\n")
+            file.write("\n>"+record.id+"|+3\n")
             file.write(str(forward[2:].translate()))
-            file.write("\n>"+sequence.id+"|-1\n")
+            file.write("\n>"+record.id+"|-1\n")
             file.write(str(reverse.translate()))
-            file.write("\n>"+sequence.id+"|-2\n")
+            file.write("\n>"+record.id+"|-2\n")
             file.write(str(reverse[1:].translate()))
-            file.write("\n>"+sequence.id+"|-3\n")
+            file.write("\n>"+record.id+"|-3\n")
             file.write(str(reverse[2:].translate()))
             file.close()
         return "Sequences have been translated."
@@ -63,4 +51,4 @@ class DNA:
             
 if __name__ == "__main__":
     x = DNA("sequences.fa")
-    print(x.translate("test"))
+    print(x.statistics())
