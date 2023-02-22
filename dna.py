1b345a68f1b000349f03656d03b31dd55cfddda4
olivergutteridge <olivergutteridge@gmail.com>
2023-02-22 17:09:58 +0000

    update statistics function into subfunctions

--- dna.py
+++ dna.py
@@ -1,15 +1,17 @@
 from Bio.Seq import Seq
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 import os
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from geneblocks import CommonBlocks
 
-class DNA:
+class Sequence:
 
-    def __init__(self, file, ftype) -> None:
-        self.records = list(SeqIO.parse(file, ftype))
+    def __init__(self, fname, ftype) -> None:
+        self.fname = fname
+        self.records = list(SeqIO.parse(fname, ftype))
 
     def __str__(self) -> str:
         output = ""
@@ -17,37 +19,57 @@ class DNA:
             output += f"{record}\n"
         return output
 
-    def statistics(self, fname):
-        os.mkdir(path = f"./{fname}")
+class DNA(Sequence):
+
+    def __init__(self, file, ftype) -> None:
+        super().__init__(file, ftype)
+        self.lengths = []
+        self.A = []
+        self.T = []
+        self.C = []
+        self.G = []
+        self.GC = []
+
+    def base_count(self):
+        for record in self.records:
+            self.lengths.append(len(record.seq))
+            self.A.append(record.seq.count("A"))
+            self.T.append(record.seq.count("T"))
+            self.C.append(record.seq.count("C"))
+            self.G.append(record.seq.count("G"))
+            self.GC.append(record.seq.count("G")+record.seq.count("C"))
+        return "bases counted"
+    
+    def base_txt(self, fname):
         output = ""
-        ids = []
-        A = []
-        T = []
-        C = []
-        G = []
-        f = open(f"./{fname}/stats.csv", "w", encoding="UTF8")
+        x = len(self.records)
+        for i in range(x):
+            output += f"{self.records[i].id}\nLength: {self.lengths[i]}\nA: {self.A[i]}   ({round(self.A[i]/self.lengths[i]*100)}%)\nT: {self.T[i]}   ({round(self.T[i]/self.lengths[i]*100)}%)\nC: {self.C[i]}   ({round(self.C[i]/self.lengths[i]*100)}%)\nG: {self.G[i]}   ({round(self.G[i]/self.lengths[i]*100)}%)\nGC: {round((self.G[i]+self.C[i])/self.lengths[i]*100)}%\n\n"
+        file = open(f"./{fname}/base_stats.txt", "w")
+        file.write(output)
+        file.close()
+        return "text file complete"
+
+    def base_csv(self, fname):
+        f = open(f"./{fname}/base_stats.csv", "w", encoding="UTF8")
         writer = csv.writer(f)
         headers = ["sequence_id", "length", "A", "T", "C", "G", "GC"]
         writer.writerow(headers)
-        for record in self.records:
-            ids.append(record.id)
-            length = len(record.seq)
-            a = record.seq.count("A")
-            t = record.seq.count("T")
-            c = record.seq.count("C")
-            g = record.seq.count("G")
-            gc = g + c
-            data = [record.id, length, a, t, c, g, gc]
+        x = len(self.records)
+        for i in range(x):
+            data = [self.records[i].id, self.lengths[i], self.A[i], self.T[i], self.C[i], self.G[i], self.GC[i]]
             writer.writerow(data)
-            A.append(a)
-            T.append(t)
-            C.append(c)
-            G.append(g)
-            output += f"{record.id}\nLength: {length}\nA: {a}   ({round(a/length*100)}%)\nT: {t}   ({round(t/length*100)}%)\nC: {c}   ({round(c/length*100)}%)\nG: {g}   ({round(g/length*100)}%)\nGC: {round((g+c)/length*100)}%\n\n"
-        A = np.array(A)
-        T = np.array(T)
-        C = np.array(C)
-        G = np.array(G)
+        return "csv file complete"
+    
+    def base_graph(self, fname):
+        ids = []
+        x = len(self.records)
+        for i in range(x):
+            ids.append(self.records[i].id)
+        A = np.array(self.A)
+        T = np.array(self.T)
+        C = np.array(self.C)
+        G = np.array(self.G)
         plt.figure(figsize = (15,7), dpi = 600)
         plt.bar(ids, A, color = "navy")
         plt.bar(ids, T, bottom = A, color = "lime")
@@ -56,17 +78,34 @@ class DNA:
         plt.xlabel("Sequence IDs")
         plt.ylabel("Base Frequency")
         plt.legend(["A", "T", "C", "G"])
-        plt.savefig(f"./{fname}/base_graph.png")
-        file = open(f"./{fname}/summary.txt", "w")
-        file.write(output)
-        file.close()
+        plt.xticks(rotation=45, fontsize = 5)
+        plt.savefig(f"./{fname}/base_frequency_graph.png")
+        return "base frequency graph complete"
+
+    def ORF_graph(self, fname):
+        for record in self.records:
+            forward = Seq(record.seq)
+            reverse = forward.reverse_complement()
+            plus1 = forward.translate()
+            plus2 = forward[1:].translate()
+            plus3 = forward[2:].translate()
+            minus1 = reverse.translate()
+            minus2 = reverse[1:].translate()
+            minus3 = reverse[2:].translate()
+                    
+    def statistics(self, fname):
+        print(f"Starting statistical analysis of {self.fname}")
+        os.mkdir(path = f"./{fname}")
+        self.base_count()
+        self.base_txt(fname=fname)
+        self.base_csv(fname=fname)
+        self.base_graph(fname=fname)
         return "Statistical analysis complete"
     
     def geneblocks(self, fname):
         sequences = {}
         for record in self.records:
             sequences[f"{record.id}"]=f"{record.seq}"
-        print(sequences)
         common_blocks = CommonBlocks.from_sequences(sequences)
         ax = common_blocks.plot_common_blocks()
         ax[0].figure.savefig(f"{fname}.png", bbox_inches = "tight")
@@ -78,23 +117,18 @@ class DNA:
         for record in self.records:
             forward = Seq(record.seq)
             reverse = forward.reverse_complement()
+            plus1 = forward.translate()
+            plus2 = forward[1:].translate()
+            plus3 = forward[2:].translate()
+            minus1 = reverse.translate()
+            minus2 = reverse[1:].translate()
+            minus3 = reverse[2:].translate()
+            translations = [SeqRecord(seq = Seq(plus1), id = f"{record.id}", description = "+1 translation"), SeqRecord(seq = Seq(plus2), id = f"{record.id}", description = "+2 translation"), SeqRecord(seq = Seq(plus3), id = f"{record.id}", description = "+3 translation"), SeqRecord(seq = Seq(minus1), id = f"{record.id}", description = "-1 translation"), SeqRecord(seq = Seq(minus2), id = f"{record.id}", description = "-2 translation"), SeqRecord(seq = Seq(minus3), id = f"{record.id}", description = "-3 translation")]
             file = open(f"./{fname}/{record.id}.fa", "w")
-            file.write(">"+record.id+"|+1\n")
-            file.write(str(forward.translate()))
-            file.write("\n>"+record.id+"|+2\n")
-            file.write(str(forward[1:].translate()))
-            file.write("\n>"+record.id+"|+3\n")
-            file.write(str(forward[2:].translate()))
-            file.write("\n>"+record.id+"|-1\n")
-            file.write(str(reverse.translate()))
-            file.write("\n>"+record.id+"|-2\n")
-            file.write(str(reverse[1:].translate()))
-            file.write("\n>"+record.id+"|-3\n")
-            file.write(str(reverse[2:].translate()))
+            SeqIO.write(translations, file, "fasta")
             file.close()
         return "Translation complete"
-            
-            
+                   
 if __name__ == "__main__":
-    x = DNA("sequences.fa", "fasta")
-    x.statistics("test")
+    x = DNA("hoxC_sequences.fa", "fasta")
+    print(x.statistics("test"))
