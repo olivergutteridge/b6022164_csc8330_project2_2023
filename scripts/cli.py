80c02406c99f7d04ff20b531d998e6e857d570de
olivergutteridge <olivergutteridge@gmail.com>
2023-02-24 11:04:38 +0000

    edit args

--- scripts/cli.py
+++ scripts/cli.py
@@ -9,8 +9,8 @@ def cli():
     parser.add_argument("-m", "--min-orf", help = "Minimum length of ORF sequence (aa)", action = "store", type = int, required = False, default = 90)
     parser.add_argument("-b", "--basic-stats", help = "Run basic statistical analysis on input sequences", action = "store_true", required = False, default = False)
     parser.add_argument("-c", "--complex-stats", help = "Run complex statistical analysis on input sequences", action = "store_true", required = False, default = False)
-    parser.add_argument("-t", "--translate", help = "Translate all sequences in input into all size reading frames", action = "store_true", required = False, default = False)
     parser.add_argument("-s", "--save-orfs", help = "Save ORFs to an output file (fasta)", action = "store_true", required = False, default = False)
+    parser.add_argument("-t", "--translate", help = "Translate all sequences in input into all size reading frames", action = "store_true", required = False, default = False)
     args = parser.parse_args()
 
     dna = DNA(**vars(args))
