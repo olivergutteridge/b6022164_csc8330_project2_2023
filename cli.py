6aac69432ef5d805ca497b7a752bddb8d5d5cac7
olivergutteridge <olivergutteridge@gmail.com>
2023-02-24 07:43:58 +0000

    add save-orfs

--- cli.py
+++ cli.py
@@ -4,11 +4,12 @@ import argparse
 def cli():
     parser = argparse.ArgumentParser(prog = "dnaStat", description = "A simple program that produces statistical analysis of DNA sequences")
     parser.add_argument("file", help = "Input file for analysis", action = "store", type = str)
-    parser.add_argument("odir", help = "Prefix for output directory", action = "store", type = str)
-    parser.add_argument("-o", "--orf-cutoff", help = "Minimum length of ORF sequence (aa)", action = "store", type = int, required = False, default = 30)
+    parser.add_argument("out_dir", help = "Prefix for output directory", action = "store", type = str)
+    parser.add_argument("-m", "--min-orf", help = "Minimum length of ORF sequence (aa)", action = "store", type = int, required = False, default = 90)
     parser.add_argument("-b", "--basic-stats", help = "Run basic statistical analysis on input sequences", action = "store_true", required = False, default = False)
     parser.add_argument("-c", "--complex-stats", help = "Run complex statistical analysis on input sequences", action = "store_true", required = False, default = False)
     parser.add_argument("-t", "--translate", help = "Translate all sequences in input into all size reading frames", action = "store_true", required = False, default = False)
+    parser.add_argument("-s", "--save-orfs", help = "Save ORFs to an output file (fasta)", action = "store_true", required = False, default = False)
     args = parser.parse_args()
 
     dna = DNA(**vars(args))
